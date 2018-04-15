ab7c905fca81d56ed02aaf1d9f708f4e9369ac53
Ngos <simon.pascal.ngos@accenture.com>
2018-04-15 18:41:02 +0400

    init project

---
+++ src/main/java/org/simon/pascal/pochybrid/PocHybridApplication.java
@@ -0,0 +1,12 @@
+package org.simon.pascal.pochybrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocHybridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocHybridApplication.class, args);
+	}
+}
---
+++ src/test/java/org/simon/pascal/pochybrid/PocHybridApplicationTests.java
@@ -0,0 +1,16 @@
+package org.simon.pascal.pochybrid;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PocHybridApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
